f83cbefdf57e7c8bde87b6a5e7918f313755f2bb
codeboyzhou <imzhouchen@gmail.com>
2025-06-12 17:22:21 +0800

    feat(app): Init the LifeTimerApp class

---
+++ src/life_timer_app.py
@@ -0,0 +1,27 @@
+from textual.app import App, ComposeResult
+from textual.widgets import Header, Footer
+
+
+class LifeTimerApp(App):
+    """An interesting terminal application that visualizes your life's remaining time."""
+
+    TITLE = "Life Timer"
+    """Main title for the application."""
+
+    BINDINGS = [
+        ("q", "quit", "Quit")
+    ]
+    """Key bindings for the application."""
+
+    def compose(self) -> ComposeResult:
+        """Called to add widgets to the app."""
+        yield Header(show_clock=True)
+        yield Footer()
+
+    def action_quit(self) -> None:
+        """Called when the user presses the q key (or closes the app)."""
+        self.exit()
+
+
+if __name__ == "__main__":
+    LifeTimerApp().run()

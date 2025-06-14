4921d4803fe91f011946b097d8c95eda4fe7f620
codeboyzhou <imzhouchen@gmail.com>
2025-06-14 14:29:56 +0800

    refactor(app): Improve countdown logic and app structure

--- src/life_timer_app.py
+++ src/life_timer_app.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import override
 
 from textual.app import App, ComposeResult
 from textual.widgets import Header, Footer, Button
@@ -9,7 +10,9 @@ from widgets.countdown_display import CountdownDisplay
 
 
 class LifeTimerApp(App):
-    """An interesting terminal application that visualizes your life's remaining time."""
+    """
+    An interesting terminal application that visualizes your life's remaining time.
+    """
 
     TITLE = "Life Timer"
     """Main title for the application."""
@@ -22,6 +25,7 @@ class LifeTimerApp(App):
     ]
     """Key bindings for the application."""
 
+    @override
     def compose(self) -> ComposeResult:
         """Called to add widgets to the app."""
         yield Header(show_clock=True)
@@ -31,27 +35,31 @@ class LifeTimerApp(App):
         yield CountdownDisplay()
         yield Footer()
 
-    def on_button_pressed(self, event: Button.Pressed) -> None:
-        """Called when the user presses the OK button."""
+    def _on_button_pressed(self, event: Button.Pressed) -> None:
+        """Called when the user presses buttons."""
         if event.button.id == "ok-button":
-            birthday_picker = self.query_one("#birthday_picker", BirthdayPicker)
-            if birthday_picker.value is None:
-                self.notify(message="Please select your birthday!", title="Error", severity="error")
-                return
+            self._on_ok_button_pressed()
 
-            age_input = self.query_one("#age_input", AgeInput)
-            if age_input.value is None:
-                self.notify(message="Please enter the age you want to live to!", title="Error", severity="error")
-                return
+    def _on_ok_button_pressed(self) -> None:
+        """Called when the user presses the OK button."""
+        birthday_picker = self.query_one("#birthday_picker", BirthdayPicker)
+        if birthday_picker.value is None:
+            self.notify(message="Please select your birthday!", title="Error", severity="error")
+            return
 
-            birthday_picker_values = birthday_picker.value.split("-")
-            life_end_year = int(birthday_picker_values[0]) + age_input.value
-            life_end_date = f"{life_end_year}-{birthday_picker_values[1]}-{birthday_picker_values[2]}"
-            life_end_time = datetime.strptime(life_end_date, "%Y-%m-%d").timestamp()
-            life_countdown_ms = (life_end_time - datetime.now().timestamp()) * 1000
-            countdown_display = self.query_one("#countdown_display", CountdownDisplay)
-            countdown_display.set_time(int(life_countdown_ms))
-            countdown_display.start()
+        age_input = self.query_one("#age_input", AgeInput)
+        if age_input.value is None:
+            self.notify(message="Please enter the age you want to live to!", title="Error", severity="error")
+            return
+
+        birthday_picker_values = birthday_picker.value.split("-")
+        life_end_year = int(birthday_picker_values[0]) + age_input.value
+        life_end_date = f"{life_end_year}-{birthday_picker_values[1]}-{birthday_picker_values[2]}"
+        life_end_timestamp = datetime.strptime(life_end_date, "%Y-%m-%d").timestamp()
+        life_countdown_ms = (life_end_timestamp - datetime.now().timestamp()) * 1000
+        countdown_display = self.query_one("#countdown_display", CountdownDisplay)
+        countdown_display.set_start_milliseconds(int(life_countdown_ms))
+        countdown_display.start()
 
     def action_quit(self) -> None:
         """Called when the user presses the q key (or closes the app)."""
--- src/widgets/countdown_display.py
+++ src/widgets/countdown_display.py
@@ -5,7 +5,7 @@ from textual.widgets import Digits
 class CountdownDisplay(Digits):
     """A widget to display ETA time."""
 
-    time = reactive(0)
+    start_milliseconds = reactive(0)
 
     def __init__(self):
         super().__init__(id="countdown_display")
@@ -16,14 +16,14 @@ class CountdownDisplay(Digits):
         self.update_timer = self.set_interval(interval_seconds, self.update_time)
         self.update_timer.pause()
 
-    def set_time(self, time: int):
-        self.time = time
+    def set_start_milliseconds(self, milliseconds: int):
+        self.start_milliseconds = milliseconds
 
     def update_time(self):
-        self.time -= 1
+        self.start_milliseconds -= 1
 
-    def watch_time(self, time: int):
-        self.update(str(time))
+    def watch_start_milliseconds(self, milliseconds: int):
+        self.update(str(milliseconds))
 
     def start(self):
         self.update_timer.resume()
